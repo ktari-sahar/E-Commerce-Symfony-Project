e3a58391bb82d85d3898a97cbe5afaac0314f453
ktari-sahar <saharktari510@gmail.com>
2024-11-23 19:07:41 +0100

    first commit

---
+++ src/Controller/FactureController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Facture;
+use App\Form\FactureType;
+use App\Entity\Detailfacture;
+use App\Repository\FactureRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+#[Route('/facture')]
+final class FactureController extends AbstractController
+{
+    
+
+    #[Route(name: 'app_facture_index', methods: ['GET'])]
+    public function index(FactureRepository $factureRepository): Response
+    {
+        return $this->render('facture/index.html.twig', [
+            'factures' => $factureRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_facture_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $facture = new Facture();
+        $form = $this->createForm(FactureType::class, $facture);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($facture);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_facture_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('facture/new.html.twig', [
+            'facture' => $facture,
+            'form' => $form,
+        ]);
+    }
+    #[Route('/rowfacture/{id}', name: 'app_rowfacture_delete', methods: ['POST'])]
+    public function deleterowfacture(int $id,Request $request, Detailfacture $detailfacture, EntityManagerInterface $entityManager): Response
+    {
+
+        $detailfacture = $entityManager->getRepository(Detailfacture::class)->find($id);
+            $entityManager->remove($detailfacture);
+            $entityManager->flush();
+
+        return $this->redirectToRoute('app_detailfacture_index', [], Response::HTTP_SEE_OTHER);
+    }
+    #[Route('/{id}', name: 'app_facture_show', methods: ['GET'])]
+    public function show(Facture $facture): Response
+    {
+        return $this->render('facture/show.html.twig', [
+            'facture' => $facture,
+            'showModal' => true,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_facture_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Facture $facture, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(FactureType::class, $facture);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_facture_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('facture/edit.html.twig', [
+            'facture' => $facture,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_facture_delete', methods: ['POST'])]
+    public function delete(Request $request, Facture $facture, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$facture->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($facture);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_facture_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
